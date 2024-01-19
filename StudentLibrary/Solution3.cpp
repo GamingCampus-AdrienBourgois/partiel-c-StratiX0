389ec8a61c18d604a26052cb189aed73a60ac6a8
StratiX0 <115560299+StratiX0@users.noreply.github.com>
2024-01-19 14:43:36 +0100

    exo3 essai1

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -2,14 +2,23 @@
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
+
+#include <algorithm>
+
 #ifdef COMPILE_EXERCICE_3
 
 void Solution3::SetWords(const std::vector<std::string>& _words)
 {
 	words = _words;
+	std::vector<std::string> list;
+	for (int i = 0; i < sizeof(words); i++)
+	{
+		list.push_back(words[i]);
+	}
+	words = list;
 }
 
-void Solution3::SortWords() {}
+void Solution3::SortWords() { std::sort(words.begin(), words.end()); }
 
 std::vector<std::string> Solution3::GetSortedWords() const
 {
