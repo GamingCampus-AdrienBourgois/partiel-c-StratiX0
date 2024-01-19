8935f5a05c7a51644292ba76172e6b0cf1655cf9
StratiX0 <115560299+StratiX0@users.noreply.github.com>
2024-01-19 13:23:46 +0100

    exo 1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -6,7 +6,36 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+	switch (_from)
+	{
+	case TemperatureUnits::KELVIN:
+		switch (_to)
+		{
+		case TemperatureUnits::FAHRENHEIT:
+			_value = 9/5 * (_value - 273) + 32;
+		case TemperatureUnits::CELSIUS:
+			_value = _value - 273;
+		}
+	case TemperatureUnits::CELSIUS:
+		switch (_to)
+		{
+		case TemperatureUnits::FAHRENHEIT:
+			_value = 9 / 5 * (_value) + 32;
+		case TemperatureUnits::KELVIN:
+			_value = _value + 273;
+		}
+	case TemperatureUnits::FAHRENHEIT:
+		switch (_to)
+		{
+		case TemperatureUnits::CELSIUS:
+			_value = 5 / 9 * (_value - 32);
+		case TemperatureUnits::KELVIN:
+			_value = 5 / 9 * (_value - 32) + 273;
+		}
+	}
+	
+
+	return _value;
 }
 
 #endif
